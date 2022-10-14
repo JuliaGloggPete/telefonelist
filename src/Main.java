b21d4cd823de1ae14ff545236aba5d73498193c7
Julia Petersson <julia.gloggnitzer@outlook.com>
2022-10-14 10:38:39 +0200

    Made a method to search for the telephonenumber by name

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,40 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+
+        Map<String,String> telephoneList = new HashMap<>();
+
+        telephoneList.put("Greta", "1233 5987 654");
+        telephoneList.put("Hans", "979 59 87654");
+        telephoneList.put("Häxan", "898 9800");
+
+
+        System.out.println("Hello whos number are you searching for?");
+        String searchedPerson = sc.nextLine();
+        searchByName(searchedPerson,telephoneList);
 
     }
+
+    public static void searchByName(String searchedPerson, Map <String,String> telephoneList){
+
+        for(String key: telephoneList.keySet()){
+
+            if (key.equals(searchedPerson)){
+
+                System.out.println("Name "+key+" number: "+ telephoneList.get(key));
+
+            }
+
+
+        }
+    }
+
+
+
+
 }
